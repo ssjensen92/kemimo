9fc6cae2247aac01e8d32d84eb577acce47f5a35
ssjensen92 <sigurdsjensen@gmail.com>
2025-02-28 10:25:26 +0100

    fix typo.

--- models/standard/pathway.py
+++ models/standard/pathway.py
@@ -8,7 +8,7 @@ sys.path.insert(0, "./src_py/")
 from database import database
 
 #create a database from data files
-db = database(datadir="./data_deuterated_total/", nlayers=2, layerThickness=4.0)
+db = database(datadir="./data_kida2024/", nlayers=2, layerThickness=4.0, H2spin=False)
 
 #save species data to a file
 #db.showSpecies(fileName="species.out")
